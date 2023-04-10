6ed1ef067b371efe409b2d49baf80a369d5c953e
koha90 <koha90@github.com>
2023-04-10 15:13:35 +0500

    ... with request in terminal

--- internal/httpserver/simple_http_server.go
+++ internal/httpserver/simple_http_server.go
@@ -6,14 +6,23 @@ import (
 	"net/http"
 )
 
+// StringHandler ...
 type StringHandler struct {
 	message string
 }
 
+// ServeHTTP method realized
 func (sh StringHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/favicon.ico" {
+		Printfln("Request for icon detected - returning 404")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	Printfln("Request for %v", r.URL.Path)
 	io.WriteString(w, sh.message)
 }
 
+// Start ...
 func Start() {
 	err := http.ListenAndServe(":5000", StringHandler{message: "Hello, World!"})
 	if err != nil {
